7febfbd0006d0f79a6029984fb713ade909b980b
Tilman Hornung (t1h0) <64684735+t1h0@users.noreply.github.com>
2024-09-16 16:26:03 +0200

    Add multi label classifier

--- src/nomopytools/ml/nlp.py
+++ src/nomopytools/ml/nlp.py
@@ -1,17 +1,21 @@
 import torch
+from torch import nn
 from torch.utils.data import DataLoader
 from transformers import (
     AutoTokenizer,
+    AutoModel,
     AutoModelForTextEncoding,
     AutoModelForSequenceClassification,
 )
-from transformers.modeling_outputs import SequenceClassifierOutput
+from transformers.modeling_outputs import ModelOutput, SequenceClassifierOutput
 from sklearn.metrics import matthews_corrcoef, f1_score
 from tqdm import tqdm
 from bidict import bidict
+from dataclasses import dataclass
 from collections.abc import Sequence, Hashable
 import math
-from .utils import batched, convert_labels
+from typing import TypeVar, Generic
+from .utils import batched, convert_labels, one_hot_multitask
 from .transformer import Transformer, BaseAutoModel, logger
 
 
@@ -229,3 +233,183 @@ class SequenceClassifier(TextTransformer):
 
             logger.info(f"test f1 score: {f1}")
             logger.info(f"test MCC: {mcc}")
+
+
+OutputHeadName = TypeVar("OutputHeadName", bound=str)
+OutputHeadKey = TypeVar("OutputHeadKey", bound=str)
+
+
+class MultiLabelSequenceClassifier(SequenceClassifier, Generic[OutputHeadName]):
+
+    @dataclass
+    class MultiLabelOutput(ModelOutput, Generic[OutputHeadKey]):
+        """Dataclass for multi-label output."""
+
+        loss: torch.FloatTensor | None = None
+        losses: dict[OutputHeadKey, torch.FloatTensor] | None = None
+        logits: dict[OutputHeadKey, torch.FloatTensor] | None = None
+        hidden_states: tuple[torch.FloatTensor, ...] | None = None
+        attentions: tuple[torch.FloatTensor, ...] | None = None
+
+    def __init__(
+        self,
+        model_name: str,
+        model_kwargs: dict | None = None,
+        tokenizer_kwargs: dict | None = None,
+        device: torch.device | None = None,
+        freeze_model: bool = False,
+        heads: dict[OutputHeadName, int] | None = None,
+        *args,
+        **kwargs,
+    ) -> None:
+        """Multi-label classifier.
+
+        Args:
+            model_name (str): The pretrained model to use.
+            model_kwargs (dict | None, optional): kwargs to pass to the model.
+                Defaults to None.
+            tokenizer_kwargs (dict | None, optional): kwargs to pass to the tokenizer.
+                Defaults to None.
+            device (torch.device | None, optional): Torch device to use. If None, will
+                select GPU if possible, else CPU. Defaults to None.
+            freeze_model (bool, optional): Whether to freeze the pretrained model
+                (e.g. for fine-tuning). Defaults to False.
+            heads (dict[OutputHeadName, int] | None, optional): Output heads to add.
+                Keys will be identifiers in output, values the size of the respective heads.
+                Defaults to None.
+            *args, **kwargs: To pass to nn.Module.
+        """
+        if heads is None or len(heads) < 2:
+            raise ValueError("Need at least two heads for MultiLabel classification.")
+        super().__init__(
+            model_name=model_name,
+            model_kwargs=model_kwargs,
+            tokenizer_kwargs=tokenizer_kwargs,
+            auto_model_class=AutoModel,
+            device=device,
+            freeze_model=freeze_model,
+            *args,
+            **kwargs,
+        )
+
+        self.dropout = nn.Dropout(0.1)
+
+        self.heads = heads
+
+        if heads:
+            # set heads and corresponding loss functions
+            for head_name, head_size in heads.items():
+                setattr(
+                    self,
+                    f"{head_name}_head",
+                    nn.Linear(self.model.config.hidden_size, head_size),
+                )
+                setattr(self, f"{head_name}_loss_fn", nn.CrossEntropyLoss())
+
+        self.to(self.device)
+
+    def forward(self, *args, **kwargs) -> MultiLabelOutput[OutputHeadName]:
+
+        labels = kwargs.pop("labels").to(self.device)
+        output = super().forward(*args, **kwargs)
+
+        hidden_states = output.hidden_states
+        attentions = output.attentions
+        pooled_output = output.pooler_output  # CLS token representation
+        del output  # to avoid OOME
+
+        pooled_output = self.dropout(pooled_output)
+
+        # get logits from heads
+        logits = {
+            head: getattr(self, f"{head}_head")(pooled_output) for head in self.heads
+        }
+        del pooled_output  # to avoid OOME
+
+        # get losses
+        losses = {
+            head: getattr(self, f"{head}_loss_fn")(logit, task_labels)
+            for (head, logit), task_labels in zip(
+                logits.items(), labels.transpose(0, 1)
+            )
+        }
+
+        return self.MultiLabelOutput(
+            loss=sum(losses.values()),
+            losses=losses,
+            logits=logits,
+            hidden_states=hidden_states,
+            attentions=attentions,
+        )
+
+    def infer(self, *args, **kwargs) -> MultiLabelOutput:
+        return super().infer(*args, **kwargs)
+
+    # Inference function to get probabilities
+    def predict(self, *args, **kwargs) -> dict[OutputHeadName, torch.Tensor]:
+        output = self.infer(*args, **kwargs)
+
+        # Apply softmax to get probabilities
+        return {
+            head_name: logit.softmax(-1) for head_name, logit in output.logits.items()
+        }
+
+    def eval_epoch(
+        self,
+        data: DataLoader,
+        data_keys: Sequence[str],
+        forward_kwargs: dict | None = None,
+        eval_type: str = "evaluation",
+    ) -> None:
+        """Evaluates one epoch using loss as metric.
+
+        Args:
+            data (DataLoader): The data to evaluate on.
+            data_keys (Sequence[str]): Keys of each data Tensor that the dataloader yields
+                to pass it to forward call with.
+            forward_kwargs (dict | None, optional): Additional kwargs to pass to
+                forward call. If None will pass no additional kwargs. Defaults to None.
+            eval_type (str, optional): The type of evaluation (for logging).
+                Defaults to "evaluation".
+        """
+        self.eval()
+
+        f1 = 0
+
+        for batch in tqdm(
+            data,
+            desc=f"Batch {eval_type}",
+            unit="batch",
+            position=1,
+            leave=False,
+        ):
+
+            prediction = self.predict(
+                **dict(zip(data_keys, batch)),
+                **(forward_kwargs or {}),
+            )
+
+            # apply argmax and stack
+            prediction = torch.stack(
+                tuple(t.argmax(1) for t in prediction.values()), 1
+            ).cpu()
+
+            # get true labels
+            true_labels = batch[data_keys.index("labels")]
+
+            # create one hot tensors
+            prediction_one_hot = one_hot_multitask(
+                prediction, list(self.heads.values())
+            )
+            true_labels_one_hot = one_hot_multitask(
+                true_labels, list(self.heads.values())
+            )
+
+            f1 += f1_score(
+                true_labels_one_hot,
+                prediction_one_hot,
+                average="weighted",
+                zero_division=0,
+            )
+
+        logger.info(f"{eval_type} f1 score: {round(f1/len(data),2)}")
